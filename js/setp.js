eaec2096dd504bb5ed2d45fd08724b8203dc713f
Saliou <macbookpro@192.168.1.7>
2024-01-05 13:27:49 +0000

    ajout

--- js/setp.js
+++ js/setp.js
@@ -1,12 +1,17 @@
+/// etape d'insciption
+
+
 var form_1 = document.querySelector(".form_1");
 var form_2 = document.querySelector(".form_2");
 var form_3 = document.querySelector(".form_3");
 var form_4 = document.querySelector(".form_4");
+var form_5 = document.querySelector(".form_5");
 
 var form_1_btns = document.querySelector(".form_1_btns");
 var form_2_btns = document.querySelector(".form_2_btns");
 var form_3_btns = document.querySelector(".form_3_btns");
 var form_4_btns = document.querySelector(".form_4_btns");
+var form_5_btns = document.querySelector(".form_5_btns");
 
 var form_1_next_btn = document.querySelector(".form_1_btns .btn_next");
 var form_2_back_btn = document.querySelector(".form_2_btns .btn_back");
@@ -14,10 +19,19 @@ var form_2_next_btn = document.querySelector(".form_2_btns .btn_next");
 var form_3_back_btn = document.querySelector(".form_3_btns .btn_back");
 var form_3_next_btn = document.querySelector(".form_3_btns .btn_next");
 var form_4_back_btn = document.querySelector(".form_4_btns .btn_back");
+var form_4_next_btn = document.querySelector(".form_4_btns .btn_next");
+var form_5_back_btn = document.querySelector(".form_5_btns .btn_back");
 
 var form_2_progessbar = document.querySelector(".form_2_progessbar");
 var form_3_progessbar = document.querySelector(".form_3_progessbar");
 var form_4_progessbar = document.querySelector(".form_4_progessbar");
+var form_5_progessbar = document.querySelector(".form_5_progessbar");
+
+
+
+
+
+
 
 var btn_done = document.querySelector(".btn_done");
 var modal_wrapper = document.querySelector(".modal_wrapper");
@@ -83,8 +97,33 @@ form_4_back_btn.addEventListener("click", function(){
     form_4_progessbar.classList.remove("active");
 });
 
+form_4_next_btn.addEventListener("click", function(){
+    form_4.style.display = "none";
+    form_5.style.display = "block";
+
+    form_4_btns.style.display = "none";
+    form_5_btns.style.display = "flex";
+
+    form_5_progessbar.classList.add("active");
+});
+
+form_5_back_btn.addEventListener("click", function(){
+    form_5.style.display = "none";
+    form_4.style.display = "block";
+
+    form_5_btns.style.display = "none";
+    form_4_btns.style.display = "flex";
+
+    form_5_progessbar.classList.remove("active");
+});
+
+
 btn_done.addEventListener("click", function(){
     modal_wrapper.classList.add("active");
+	setTimeout(() => {
+		
+		window.location.href = "index.html";
+	  }, 300);
 
 });
 
@@ -93,7 +132,7 @@ shadow.addEventListener("click", function(){
 });
 
 
-
+/// partie selectionner de input
 
 const userTypeSelect = document.getElementById("user_type");
 const userSexeInput = document.getElementById("user_sexe_input");
@@ -107,8 +146,8 @@ const userCom = document.getElementById("user_com_input")
 const userMena1 = document.getElementById("user_mena1_input")
 const userMena2 = document.getElementById("user_mena2_input")
 
-// Fonction pour afficher ou masquer les éléments en fonction de l'option sélectionnée
-function showInputFields(sexe, age, status, compSize, serv, orgtyp1, orgtyp2, com, mena1, mena2) {
+
+function showInputFields1(sexe, age, status, compSize, serv, orgtyp1, orgtyp2, com, mena1, mena2) {
     userSexeInput.style.display = sexe;
     userAgeInput.style.display = age;
     userStatus.style.display = status;
@@ -122,30 +161,161 @@ function showInputFields(sexe, age, status, compSize, serv, orgtyp1, orgtyp2, co
 
 }
 
-// Définir les champs d'entrée par défaut lors du chargement de la page
-showInputFields("block", "block", "block", "none", "none", "none", "none", "none", "none");
+showInputFields1("block", "block", "block", "none", "none", "none", "none", "none", "none");
 
 userTypeSelect.addEventListener("change", (event) => {
     const selectedOption = event.target.value;
 
     switch (selectedOption) {
         case "option1":
-            showInputFields("block", "block", "block", "none", "none", "none", "none", "none", "none", "none");
+            showInputFields1("block", "block", "block", "none", "none", "none", "none", "none", "none", "none");
             break;
         case "option2":
-            showInputFields("none", "none", "none", "block", "none", "none", "none", "none", "none", "none");
+            showInputFields1("none", "none", "none", "block", "none", "none", "none", "none", "none", "none");
             break;
         case "option3":
-            showInputFields("none", "none", "none", "none", "block", "none", "none", "none", "none", "none");
+            showInputFields1("none", "none", "none", "none", "block", "none", "none", "none", "none", "none");
             break;
         case "option4":
-            showInputFields("none", "none", "none", "none", "none", "block", "block", "none", "none", "none");
+            showInputFields1("none", "none", "none", "none", "none", "block", "block", "none", "none", "none");
             break;
 		case "option5":
-			showInputFields("none", "none", "none", "none", "none", "none", "none", "block", "none", "none");
+			showInputFields1("none", "none", "none", "none", "none", "none", "none", "block", "none", "none");
+			break;
 		case "option6":
-			showInputFields("none", "none", "none", "none", "none", "none", "none", "none", "block", "block")
+			showInputFields1("none", "none", "none", "none", "none", "none", "none", "none", "block", "block");
+			break;
         default:
             // Gérer d'autres options au besoin
     }
 });
+
+const accountType = document.getElementById("account_type");
+const actorType = document.getElementById("actor_type_input");
+const actorType2 = document.getElementById("actor_type_input2");
+const actorBudginput = document.getElementById("actor_budg_input");
+const agentAccount = document.getElementById("agent_account_input")
+
+
+
+
+function showInputFields2(actor, actor2, actorBudg, agent, ){
+	actorType.style.display = actor;
+	actorType2.style.display = actor2;
+    actorBudginput.style.display = actorBudg;
+	agentAccount.style.display = agent;
+	
+}
+
+ showInputFields2("block", "none", "none", "none");
+accountType.addEventListener("change", (event) =>{
+	const selectedOption = event.target.value;
+	switch (selectedOption){
+		case "option1":
+		case "option2":
+			showInputFields2("block", "none", "none", "none");
+			break;
+		case "option3":
+			showInputFields2("none", "block", "none", "none");
+			break;
+		case "option4":
+			showInputFields2("none", "none", "block", "none");
+			break;
+		case "option5":
+			showInputFields2("none", "none", "none", "block");
+			break;
+		
+		default:
+			
+	}
+});
+
+
+
+
+    const sectorActivitySelector = document.getElementById("sector_activity_selector");
+    const industrySelector = document.getElementById("industry_selector");
+    const buildingTypeInput = document.getElementById("building_type_input");
+    const commerceSectorSelector = document.getElementById("commerce_sector_selector");
+    const transportSectorSelector = document.getElementById("transport_sector_selector");
+
+    function showInputFields(selector, displayValue) {
+        selector.style.display = displayValue;
+    }
+
+    showInputFields(industrySelector, "block");
+    showInputFields(buildingTypeInput, "none");
+    showInputFields(commerceSectorSelector, "none");
+    showInputFields(transportSectorSelector, "none");
+
+    sectorActivitySelector.addEventListener("change", (event) => {
+        const selectedOption = event.target.value;
+
+        // Hide all selectors initially
+        showInputFields(industrySelector, "none");
+        showInputFields(buildingTypeInput, "none");
+        showInputFields(commerceSectorSelector, "none");
+        showInputFields(transportSectorSelector, "none");
+
+        // Show the relevant selector based on the selected option
+        switch (selectedOption) {
+            case "option1":
+                showInputFields(industrySelector, "block");
+                break;
+            case "option2":
+                showInputFields(buildingTypeInput, "block");
+                break;
+            case "option3":
+                showInputFields(commerceSectorSelector, "block");
+                break;
+            case "option4":
+                showInputFields(transportSectorSelector, "block");
+                break;
+            default:
+                // Do nothing for other options
+        }
+    });
+
+
+	fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(data => {
+        const countryDropdown = document.getElementById('country');
+
+        // Ajouter la Côte d'Ivoire en tant que première option
+        const optionIvoryCoast = document.createElement('option');
+        optionIvoryCoast.value = 'option1';
+        optionIvoryCoast.textContent = 'Côte d\'Ivoire';
+        countryDropdown.appendChild(optionIvoryCoast);
+
+        // Ajouter les autres pays
+        data.forEach((country, index) => {
+            const option = document.createElement('option');
+            option.value = 'option' + (index + 2);
+            option.textContent = country.name.common;
+            countryDropdown.appendChild(option);
+        });
+    })
+    .catch(error => console.error('Erreur lors de la récupération des pays', error));
+
+
+	function populateCountryDropdown() {
+        const countryDropdown = document.getElementById('country_code');
+        fetch('https://restcountries.com/v2/all')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(country => {
+                    if (country.hasOwnProperty('callingCodes') && country.callingCodes.length > 0) {
+                        const countryCode = country.callingCodes[0];
+                        const countryName = country.name;
+                        const option = document.createElement('option');
+                        option.value = countryCode;
+                        option.textContent = `${countryName} (+${countryCode})`;
+                        countryDropdown.appendChild(option);
+                    }
+                });
+            })
+            .catch(error => console.error('Erreur lors de la récupération des pays', error));
+    }
+
+    populateCountryDropdown();
